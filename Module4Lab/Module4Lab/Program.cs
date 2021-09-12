34848e782b24181017c284232f3ef1b13d63bd0a
Andrew <andrewmgunn26@gmail.com>
2021-09-11 20:04:28 -0500

    ADS

---
+++ Module4Lab/Module4Lab/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Module4Lab
+{
+    class Program
+    {
+
+      
+        
+        static void Main(string[] args)
+        {
+
+            // double total = 0;
+            //
+            // int days = 10;
+            //
+            // for (int i = ; i <= days; i++)
+            // {
+            //
+            //     if (days == 1)
+            //     {
+            //         total += 1;
+            //         Console.WriteLine($"Pennies earned on day 1: {total}");
+            //     } else if (days == 2)
+            //
+            //     {
+            //        
+            //         total += 2;
+            //         Console.WriteLine($"Pennies earned on day 2: {total}");
+            //     }
+            //     else
+            //     {
+            //         total += total * 2;
+            //         Console.WriteLine($"Pennies earned on day {days}: {total}");
+            //
+            //     }
+            //
+            // }
+            //
+            // days++;
+            // return total;
+
+            int count = 4;
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
